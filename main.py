7f5263ed03f93bb21011b09aed68d8650792e48b
coding-is-FCKING-TOURTUR3 <Object-Oriented-Idiot@outlook.com>
2026-08-20 15:36:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@
 #random shi idk
 
 import scrypt
-import secrets
+import getpass
+from functools import wraps
 
-def check_password(password)
+def check_password(password):
     UI = str(input('Please input the password needed to view this file.'))
     if UI == password:
         return True
@@ -15,71 +16,41 @@ def check_password(password)
         print('Incorrect password.')
         return False
 
-def format(function, password) 
-    if function.__name__ == decrypt:
-        if check_password(password):
-            print('decryption process started- this may take some time.')
-    else:
-        print('encryptoin process started- this may take some time.')
-    def inner():
-        return function()
+def format_(function):
+    @wraps(function)    
+    def inner(self):
+        print(f'{function.__name__.capitalize()}ion process started- this may take some time.')
+        result = function(self)
+        print(f'{function.__name__.capitalize()}ion process finished- thank you for waiting.')
+        return result
     return inner
 
 
-class idk(filename):
+class FileConverter():
     def __init__(self, filename):
-        self.filename = filename
-        self.password = str(input('Please enter a password you can use to accsess your file.')
-            if !(len(self.password)):
-                self.password = str(input('Please enter a password you can use to accsess your file.')
+        self.filename = filename 
+        self.password = getpass.getpass('Please enter a password you can use to accsess your file.')
+        if not self.password:
+            self.password = getpass.getpass('Please enter a password you can use to accsess your file.')
         
-    @format(self.password)
+    @format_
     def decrypt(self):
-        def upd(self):
-            file = open(self.filename, 'rb')
-            contents = []
-            new_contents = []
-            contents = file.read()
-            file.close()
+        if check_password(self.password):
+            with open(self.filename, 'rb') as file:
+                contents = file.read()
             if len(contents):
-                 new_contents = decrypt_(contents)
+                 new_contents = scrypt.decrypt(contents, self.password)
             else:
                 print('file seems to be empty')
-            file = open(filename, 'wb')
-            for item in new_contents:
-                item = item.encode("utf-8")
-                file.write(item)
-            file.close()
+            with open(self.filename, 'wb') as file:
+                file.write(new_contents)
 
-        def decrypt_(self):
-            result = []
-            result.append(scrypt.decrypt(self.filename, self.password))
-            return result
-        upd('file.txt')
-        print('decyption process finished, ty for waiting!')
 
-    @format
-    def encrypt(file):
-        def upd(filename):
-            print('encryption process started- this make take a bit of time')
-            file = open(filename, 'rb')
-            contents = []
-            for line in file:
-                contents.append(line)
-            file.close()
-            new_contents = encrypt_(contents)
-            file = open(filename, 'wb')
-            for item in new_contents:
-                file.write(item)
-            file.close()
+    @format_
+    def encrypt(self):
+        with open(self.filename, 'rb') as file:
+                contents = file.read()
+        new_contents = scrypt.encrypt(contents, self.password)
+        with open(self.filename, 'wb') as file:
+            file.write(new_contents)
     
-        def encrypt_(victim):
-            result = []
-            for item in victim:
-                item = scrypt.encrypt(item, salt)
-                result.append(item)
-            return result
-    
-        upd('file.txt')
-        print('encryption process finished, ty for waiting!')
-
